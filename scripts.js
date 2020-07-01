1ef7bb6531a760683d0f420e2c49e53af9512dc9
Leila <leilaapsilva26@gmail.com>
2020-06-30 23:33:34 -0300

    Create menu scene and start button

--- scripts.js
+++ scripts.js
@@ -1,26 +1,52 @@
-const num_max = 20; //numero do maior card
+function menuScene(){
+  var startButton = document.createElement("button");
+  startButton.setAttribute("id", startButton);
+  startButton.innerHTML = "Start";
 
-var cardsList = []; //lista de cards
-console.log("cardslist: ", cardsList);
+  var menuDiv = document.createElement("div");
 
-//Função que gera o número do card a ser adicionado
-function getCardNumber(){
+  menuDiv.appendChild(startButton);
 
-  number = Math.floor((Math.random() * num_max)) + 1; //random de 1 a num_max
+  var section = document.getElementById("section");
+  section.appendChild(menuDiv);
 
-  while(cardsList.includes(number)){ //elimina cartas repetidas
-    number = Math.floor((Math.random() * num_max)) + 1;
-  }  
-
-  strNumber = ("00" + number).slice(-3) //transforma no formato "001" - "999"
-
-  cardsList.push(number); //adiciona o card na lista
-
-  return strNumber;
+  startButton.addEventListener ("click", function() {
+    menuDiv.remove();
+    gameScene();
+  });
 }
 
-//adiciona dois cards com um personagem e uma imagem de verso
-function addCard() { 
+menuScene();
+
+//Inicia o jogo
+//gameScene()
+
+//Cena do jogo em si
+function gameScene() {
+
+  const num_max = 20; //numero do maior card
+
+  var cardsList = []; //lista de cards
+  console.log("cardslist: ", cardsList);
+
+  //Função que gera o número do card a ser adicionado
+  function getCardNumber() {
+
+    number = Math.floor((Math.random() * num_max)) + 1; //random de 1 a num_max
+
+    while (cardsList.includes(number)) { //elimina cartas repetidas
+      number = Math.floor((Math.random() * num_max)) + 1;
+    }
+
+    strNumber = ("00" + number).slice(-3) //transforma no formato "001" - "999"
+
+    cardsList.push(number); //adiciona o card na lista
+
+    return strNumber;
+  }
+
+  //adiciona dois cards com um personagem e uma imagem de verso
+  function addCard() {
 
     var cardNumber = getCardNumber();
 
@@ -35,7 +61,7 @@ function addCard() {
     // CARD 1
 
     //cria imagem do personagem
-    var newImgFront = document.createElement("img"); 
+    var newImgFront = document.createElement("img");
     newImgFront.className = "front-face";
     newImgFront.src = 'img/' + cardNumber + '.png';
 
@@ -44,14 +70,14 @@ function addCard() {
     newImgBack.className = "back-face";
     newImgBack.src = 'img/back.png';
 
-     //adiciona imagens à div
-     newDiv.appendChild(newImgFront);
-     newDiv.appendChild(newImgBack);
+    //adiciona imagens à div
+    newDiv.appendChild(newImgFront);
+    newDiv.appendChild(newImgBack);
 
     // CARD 2
 
     //cria imagem do personagem
-    var newImgFront2 = document.createElement("img"); 
+    var newImgFront2 = document.createElement("img");
     newImgFront2.className = "front-face";
     newImgFront2.src = 'img/' + cardNumber + '.png';
 
@@ -68,74 +94,76 @@ function addCard() {
     section.appendChild(newDiv);
     section.appendChild(newDiv2);
     console.log(section);
-}
-
-// adiciona 12 pares de cards
-for(i = 0; i < 12; i++){
-  x = addCard()
-}
-
-//seleciona todos os cards
-const cards = document.querySelectorAll('.memory-card');
-
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-
-function flipCard() { //vira card
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flip');
-
-  if (!hasFlippedCard) {
-    // first click
-    hasFlippedCard = true;
-    firstCard = this;
-
-    return;
   }
 
-  // second click
-  secondCard = this;
+  // adiciona 12 pares de cards
+  for (i = 0; i < 12; i++) {
+    x = addCard()
+  }
 
-  checkForMatch(); //verifica se é igual
-}
+  //seleciona todos os cards
+  const cards = document.querySelectorAll('.memory-card');
 
-function checkForMatch() {
-  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
 
-  isMatch ? disableCards() : unflipCards();
-}
+  function flipCard() { //vira card
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-function disableCards() { //quando o card já está correto, é desabilitado
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
+    this.classList.add('flip');
 
-  resetBoard();
-}
+    if (!hasFlippedCard) {
+      // first click
+      hasFlippedCard = true;
+      firstCard = this;
 
-function unflipCards() { //desvira o card, quando está errado
-  lockBoard = true;
+      return;
+    }
 
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+    // second click
+    secondCard = this;
+
+    checkForMatch(); //verifica se é igual
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+  }
+
+  function disableCards() { //quando o card já está correto, é desabilitado
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
-  }, 1500);
+  }
+
+  function unflipCards() { //desvira o card, quando está errado
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  (function shuffle() { //embaralha os cards no início do jogo
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 12);
+      card.style.order = randomPos;
+    });
+  })();
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
+
 }
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() { //embaralha os cards no início do jogo
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
