5c15794d819837f81ec213a50d8000ab08995731
Leila <leilaapsilva26@gmail.com>
2020-07-02 14:31:26 -0300

    Test loading page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
 
+menuScene();
+
+//aguarda o carregamento da pagina
+function onReady(callback){
+  var intervalId = window.setInterval(function(){
+    if (document.getElementsByTagName('body')[0] !== undefined){
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible){
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function(){
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
+
+
 
 function menuScene(){
 
@@ -35,7 +57,6 @@ function menuScene(){
   });
 }
 
-menuScene();
 
 //Inicia o jogo
 //gameScene()
